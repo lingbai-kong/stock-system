ca862af34ed79ca736c50cecc3a6b0c66f2e5170
Hundred KL <59964470+lingbai-kong@users.noreply.github.com>
2021-07-25 19:04:50 +0800

    Add files via upload

---
+++ 前端工程/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		globalData:{
+			kind:'',
+			uid:'',
+			jwt:''
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
